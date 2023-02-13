8dfd37b421d9f3780eb591a75022ae2e3f49ce2b
Sohan Singh <58332895+sohansingh-blaze@users.noreply.github.com>
2023-02-13 12:21:41 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily:{
+      'Fantasy':['Fantasy'],
+      'serif':['Times New Roman'],
+      'Courier':['Courier'],
+      'mono':['monospace']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
